0ebe9c021d56539f6e36db1418e6a8b4fdb8ff13
Quinn McHenry <quinn@jqm.us>
2019-06-28 05:39:36 -0400

    Initial commit

---
+++ Kontinuierlich/ContentView.swift
@@ -0,0 +1,34 @@
+//
+//  ContentView.swift
+//  Kontinuierlich
+//
+//  Created by Quinn McHenry on 6/27/19.
+//  Copyright © 2019 Quinn McHenry. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    @State private var selection = 0
+ 
+    var body: some View {
+        TabbedView(selection: $selection){
+            Text("First View")
+                .font(.title)
+                .tabItemLabel(Image("first"))
+                .tag(0)
+            Text("Second View")
+                .font(.title)
+                .tabItemLabel(Image("second"))
+                .tag(1)
+        }
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
---
+++ KontinuierlichTests/KontinuierlichTests.swift
@@ -0,0 +1,34 @@
+//
+//  KontinuierlichTests.swift
+//  KontinuierlichTests
+//
+//  Created by Quinn McHenry on 6/27/19.
+//  Copyright © 2019 Quinn McHenry. All rights reserved.
+//
+
+import XCTest
+@testable import Kontinuierlich
+
+class KontinuierlichTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
